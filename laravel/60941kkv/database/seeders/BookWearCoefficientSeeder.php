817e627d50975f41c2af094a5bf94a45f451a634
0hJonny <0hJonny>
2024-09-11 22:50:37 +0500

    Seeder changed.

--- laravel/60941kkv/database/seeders/BookWearCoefficientSeeder.php
+++ laravel/60941kkv/database/seeders/BookWearCoefficientSeeder.php
@@ -14,13 +14,13 @@ class BookWearCoefficientSeeder extends Seeder
     public function run(): void
     {
         DB::table('book_wear_coefficients')->insert([
-            ['book_wear_coefficient_name' => 'Плохо'],
-            ['book_wear_coefficient_name' => 'Неудовлетворительно'],
-            ['book_wear_coefficient_name' => 'Удовлетворительно'],
-            ['book_wear_coefficient_name' => 'Хорошо'],
-            ['book_wear_coefficient_name' => 'Отлично'],
-            ['book_wear_coefficient_name' => 'Новый'],
-            ['book_wear_coefficient_name' => 'Идеальное'],
+            ['book_wear_coefficient_name' => 'Критическое'],
+            ['book_wear_coefficient_name' => 'Очень плохое'],
+            ['book_wear_coefficient_name' => 'Плохое'],
+            ['book_wear_coefficient_name' => 'Удовлетворительное'],
+            ['book_wear_coefficient_name' => 'Хорошее'],
+            ['book_wear_coefficient_name' => 'Отличное'],
+            ['book_wear_coefficient_name' => 'Новая']
         ]);
     }
 }
